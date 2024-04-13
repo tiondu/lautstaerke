f902f329f5c54aabb33db01ee68ebc207309c5e6
tiondu <166902487+tiondu@users.noreply.github.com>
2024-04-13 17:29:39 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,55 @@
+let _4digit: grove.TM1637 = null
+basic.forever(function () {
+    _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
+    _4digit.show(input.soundLevel())
+    if (input.soundLevel() <= 20) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (input.soundLevel() <= 40) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    } else if (input.soundLevel() <= 60) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() <= 80) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() <= 100) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    control.waitMicros(6000)
+})
